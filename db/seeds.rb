8dc67eb8aafc5a4624823a13321b85e7e5e2aef3
un5t0ppab13 <devbrights@gmail.com>
2015-08-31 09:09:22 -0700

    I love faker, no more manual entries

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,30 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+require 'faker'
+include Faker
+
+100.times do
+  article = Article.create( :title => Faker::Name.title, #=> "Legacy Creative Director",
+                            :body => Faker::Lorem.sentence #=> 
+                            # :body => Faker::Lorem.sentence(5) #=> 
+                          )
+  puts article.inspect
+end
+# rake db:reset db:migrate db:seed
+
+
+# below worked:
+
+# require 'faker'
+# include Faker
+
+# 100.times do
+#   article = Article.create( :title => "Example Title: #{1..100}",
+#                             :body => 'Body of knowlege'
+#                           )
+#   puts article.inspect
+# end
+# # rake db:reset
+# # rake db:seed
